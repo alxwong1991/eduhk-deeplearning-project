1c9bd18f319616eac6a8790e3c7eb4de6da80d40
Wong Sau Hang Alex <40209618+alxwong1991@users.noreply.github.com>
2024-03-26 01:08:03 +0800

    last fixes

--- modules/exercise.py
+++ modules/exercise.py
@@ -9,7 +9,6 @@ class Exercise:
     def setup_bicep_curls(self):
         with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
             self.bicep_curls = BicepCurls(pose)
-            self.bicep_curls.timer_instance.start()
 
     def perform_bicep_curls(self, frame):
         frame, angle, counter = self.bicep_curls.perform_exercise(frame)
